d2f4ac9c6616bd814e02d219c0ca0303b5236157
bernardbdas <bernarddas1@gmail.com>
2024-04-16 07:32:31 +0530

    some changes

--- api/app.py
+++ api/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from langchain.prompts import ChatPromptTemplate
-from langchain.chat_models import ChatOpenAI
+from langchain_core.prompts.chat import ChatPromptTemplate
+from langchain_community.chat_models import ChatOpenAI
 from langserve import add_routes
 import uvicorn
 import os
@@ -9,14 +9,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+# os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 
 app = FastAPI(
     title="Langchain Server", version="1.0", decsription="A simple API Server"
 )
 
-add_routes(app, ChatOpenAI(), path="/openai")
-model = ChatOpenAI()
+# add_routes(app, ChatOpenAI(), path="/openai")
+# model = ChatOpenAI()
+
 ##ollama llama2
 llm = Ollama(model="llama2")
 
@@ -27,7 +30,7 @@ prompt2 = ChatPromptTemplate.from_template(
     "Write me an poem about {topic} for a 5 years child with 100 words"
 )
 
-add_routes(app, prompt1 | model, path="/essay")
+add_routes(app, prompt1 | llm, path="/essay")
 
 add_routes(app, prompt2 | llm, path="/poem")
 
